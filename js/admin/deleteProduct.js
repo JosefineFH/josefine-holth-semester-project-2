d3d233b1aff97980b66694a8e9fc1396a52ab675
Josefine Holth <Josefine.Holth@hotmail.com>
2022-04-27 16:36:05 +0200

    add remove product function

---
+++ js/admin/deleteProduct.js
@@ -0,0 +1,27 @@
+import { baseUrl } from "../data/api.js";
+import { getToken, getUserInfo } from "../utils/storage.js";
+
+export async function removeProduct(id){
+  const doDelete = confirm("Do you want to delete this product?");
+  const errorMessage = document.querySelector(".message");
+
+  if(doDelete){
+    const url = baseUrl + "products/" + id;
+    const token = getToken();
+
+    const options = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    
+    try {
+      const response = await fetch(url, options);
+    } catch (error) {
+      errorMessage.innerHTML = "<p>Something whent wrong when you ware trying to delete this.</p>"
+    }
+
+  }
+
+}
