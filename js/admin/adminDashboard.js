4ae38094f64c3545e25cb366e7eb1603e1addbec
Josefine Holth <Josefine.Holth@hotmail.com>
2022-04-27 16:38:03 +0200

    add the delete product script

--- js/admin/adminDashboard.js
+++ js/admin/adminDashboard.js
@@ -1,6 +1,6 @@
 import getData from "../data/apiCall.js";
 import { getToken, getUserInfo } from "../utils/storage.js";
-
+import { removeProduct } from "./deleteProduct.js";
 
 export default function createProductList(products){
 
@@ -16,9 +16,21 @@ export default function createProductList(products){
       <h2>${title}</h2>
 
       <a class="buttons" href="/admin/editProduct.html?id=${id}">Edit</a>
+      <button class="delete__button" data-id="${id}">Delete</button>
+      
     </li>
     `
   });
-  
+  const removeButton = document.querySelectorAll(".delete__button");
 
+  
+  removeButton.forEach(button => {
+    let postId = button.dataset.id;
+    button.addEventListener("click", () => {
+      console.log(postId)
+      console.log(button)
+      removeProduct(postId)
+    })
+    
+  });
 }
