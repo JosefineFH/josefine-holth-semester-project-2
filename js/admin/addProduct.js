254953a4e9233d4e1e22642c4f997a9c39571a3d
Josefine Holth <Josefine.Holth@hotmail.com>
2022-05-09 20:32:09 +0200

    trying to fix add product function
    Get a 500 error

--- js/admin/addProduct.js
+++ js/admin/addProduct.js
@@ -1,5 +1,8 @@
 import { getToken, getUserInfo } from "../utils/storage.js";
 import { baseUrl } from "../data/api.js";
+import { createCategoryList } from "./createCategory.js";
+
+createCategoryList();
 
 const user = getUserInfo();
 const token = getToken();
@@ -10,19 +13,22 @@ if (user.length === 0 && token.length === 0) {
 
 const message = document.querySelector(".message");
 const form = document.querySelector("form");
-
 const titleInput = document.querySelector("#title");
 const descriptionInput = document.querySelector("#description");
 const featuredItemValue = document.querySelector("#featured");
 const freeItemValue = document.querySelector("#free__product");
 const priceValue = document.querySelector("#price");
-const coverInput = document.querySelector("#cover");
-const productImageInput = document.querySelector("#product__images");
-const altTextValue = document.querySelector("#alt_text");
-const selectedCategories = document.querySelector("#category");
-const loader = document.querySelector(".loader")
 
-export function getFormValue(event) {
+// Inputs for the images
+const coverInput = document.querySelector("#cover");
+const coverInputAltText = document.querySelector("#cover__altText");
+const productImageInput = document.querySelector("#product__images");
+const productImageAltTextInput = document.querySelector("#cover__altText");
+const selectedCategories = document.querySelector("#category");
+
+form.addEventListener("submit", getFormValue);
+
+async function getFormValue(event) {
   event.preventDefault();
 
   const title = titleInput.value.trim();
@@ -30,25 +36,23 @@ export function getFormValue(event) {
   const featured = featuredItemValue.checked;
   const free = freeItemValue.checked;
   const price = priceValue.value.trim();
-  const cover_image = coverInput.files;
-  const productImages = productImageInput.files
-  const altText = altTextValue.value.trim();
   const category = selectedCategories.value.trim();
+  const cover_img_url = coverInput.value.trim();
+  const cover_image_alt_text = coverInputAltText.value.trim();
+  const product_image_1 = productImageInput.value.trim();
+  const product_image_1_altText = productImageAltTextInput.value.trim();
 
-  const data = JSON.stringify({ title, description, featured, free, price, altText, category })
-
-  addProduct(data, cover_image, productImages)
+  const data = JSON.stringify({ id, title, description, featured, free, price, category, cover_img_url, cover_image_alt_text, product_image_1, product_image_1_altText })
+  addProduct(data)
 }
 
-form.addEventListener("submit", getFormValue);
+form.addEventListener("submit", addProduct)
 
-async function addProduct(data, cover_image, productImages){
-
-  const updateUrl = baseUrl + "products"
+async function addProduct(data){
+  console.log(data)
+  const updateUrl = baseUrl + "products";
   const formData = new FormData();
 
-  formData.append("files.cover_image", cover_image[0]);
-  formData.append("files.images", productImages[0])
   formData.append("data", data);
 
   const options = {
@@ -56,10 +60,11 @@ async function addProduct(data, cover_image, productImages){
     body: formData,
     headers: {
       // "Content-Type": "application/json",
-      
       Authorization: `Bearer ${token}`,
     },
   };
+
+  console.log(options)
 
   try {
     const response = await fetch(updateUrl, options);
@@ -70,7 +75,6 @@ async function addProduct(data, cover_image, productImages){
     if(json.error){
       console.log("error")
     }
-    loader.innerHTML = "Loading"
 
   } catch (error) {
     console.log(error)
