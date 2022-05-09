42bc680dad3250aaa3ce73372beed8d991a8fd02
Josefine Holth <Josefine.Holth@hotmail.com>
2022-05-09 16:09:37 +0200

    finish the shoppingCart functions

--- js/components/shoppingCart.js
+++ js/components/shoppingCart.js
@@ -1,102 +1,145 @@
-import { getFromStorage, saveProduct } from "../utils/storage.js"
+import { getFromStorage, saveProduct } from "../utils/storage.js";
 
-shoppingCart()
+shoppingCart();
 export function shoppingCart() {
-  let existingShoppingCart = getFromStorage("shopping__card")
-  const errorMessageContainer = document.querySelector(".error__message")
+  let existingShoppingCart = getFromStorage("shopping__card");
+  const errorMessageContainer = document.querySelector(".error__message");
   if (existingShoppingCart.length === 0) {
     errorMessageContainer.innerHTML = `
     <p>You dont have any items her yeat</p>
     <p>You can have a look <a href="/productPage.html">her</a></p>
-    `
+    `;
   }
-  const shoppingCartContainer = document.querySelector(".shopping__cart--list")
+  const shoppingCartContainer = document.querySelector(".shopping__cart--list");
 
-  let resultFromLocalStorage = [...existingShoppingCart.reduce(
-    (preVal, currVal) => {
-      if (!preVal.has(currVal.id))
-        preVal.set(currVal.id, {
-          ...currVal,
-          count: 0
-        });
+  let resultFromLocalStorage = [
+    ...existingShoppingCart
+      .reduce((preVal, currVal) => {
+        if (!preVal.has(currVal.id))
+          preVal.set(currVal.id, {
+            ...currVal,
+            count: 0,
+          });
 
-      preVal.get(currVal.id).count++;
-      return preVal;
-    },
-    new Map
-  ).values()];
+        preVal.get(currVal.id).count++;
+        return preVal;
+      }, new Map())
+      .values(),
+  ];
 
   let cart;
-  resultFromLocalStorage.forEach(item => {
-    const itemId = item.id
-    cart = existingShoppingCart.filter((item) => item.id === itemId).length
+  let sumPrProduct;
+  resultFromLocalStorage.forEach((item) => {
+    const itemId = item.id;
+    cart = existingShoppingCart.filter((item) => item.id === itemId).length;
 
     shoppingCartContainer.innerHTML += `
       <div class="card">
       <img src="${item.image}">
-        <h2 class="card-title">${item.title}</h2>
-       <p class="single__price">${item.price}</p>
-       <a href="/productDetails.html?id${item.id}">View Product</a> 
-       <button class="btn-primary minus__button" data-id="${item.id}">-</button>
-       <p class="product__count" data-id="${item.id}">${cart}</p>
-       <button class="btn-primary plus__button" data-id="${item.id}" data-img="${item.image}" data-price="${item.price}" data-title="${item.title}">+</button>
-
-       <p class="total__sum">Total sum: <span></span></p>
+      <h2 class="card-title">${item.title}</h2>
+      <p class="single__price"><span>${item.price}</span> €</p>
+      <a href="/productDetails.html?id${item.id}">View Product</a> 
+      <div>
+      <button class="btn-primary minus__button" data-id="${item.id}" data-price="${item.price}">-</button>
+      <p class="product__count" data-id="${item.id}" >${cart}</p>
+      <button class="btn-primary plus__button" data-id="${item.id}" data-img="${item.image}" data-price="${item.price}" data-title="${item.title}">+</button>
       </div>
-      `
-  });
-
-  const productCount = document.querySelectorAll(".product__count")
-  
-  // get the total sum
-  // get the sum for total of one product if you have more then one 
-
-
-  const minusButton = document.querySelectorAll(".minus__button")
-  minusButton.forEach(button => {
-    button.addEventListener("click", () => {
-      const deleteThisItem = button.dataset.id
-      for(let i in existingShoppingCart){
-        const id = existingShoppingCart[i].id
-        if(deleteThisItem == existingShoppingCart[i].id){
-          existingShoppingCart.findIndex( i => i.id === deleteThisItem);
-          existingShoppingCart.splice(i, 1)
-
-          productCount.forEach(count => {
-            if(count.dataset.id === id){
-              const countHTML = parseInt(count.innerText)
-              count.innerHTML = countHTML - 1
-            }
-          });
-
-          break
-        }
-      }
-      saveProduct(existingShoppingCart)
       
-    })
-  });
+      <p class="total__sum">Total sum: <span data-id="${item.id}"></span></p>
+      </div>
+      `;
 
-  const plusButton = document.querySelectorAll(".plus__button")
+    const totalSumProducts = document.querySelectorAll(".total__sum span");
 
-  plusButton.forEach(button => {
-    button.addEventListener("click", () => {    
+    const productCount = document.querySelectorAll(".product__count");
+    const minusButton = document.querySelectorAll(".minus__button");
+    const plusButton = document.querySelectorAll(".plus__button");
+    const totalShoppingSum = document.querySelector(
+      ".total__shopping--sum span"
+    );
+
+    sumPrProduct = item.price;
+    totalSumProducts.forEach((element) => {
+      if (element.dataset.id === item.id) {
+        element.innerText = cart * sumPrProduct;
+      }
+    });
+
+    let sum = 0;
+    totalSumProducts.forEach((sumPrProduct) => {
+      let sumsPrProduct = parseInt(sumPrProduct.innerHTML);
+      sum += sumsPrProduct;
+    });
+    totalShoppingSum.innerHTML = `${sum} €`;
+
+    minusButton.forEach((button) => {
+      button.addEventListener("click", () => {
+        const deleteThisItem = button.dataset.id;
+        const singleProPrice = button.dataset.price;
+
+        for (let i in existingShoppingCart) {
+          const id = existingShoppingCart[i].id;
+
+          if (deleteThisItem == existingShoppingCart[i].id) {
+            existingShoppingCart.findIndex((i) => i.id === deleteThisItem);
+            existingShoppingCart.splice(i, 1);
+
+            productCount.forEach((count) => {
+              if (count.dataset.id === id) {
+                const countHTML = parseInt(count.innerText);
+                count.innerHTML = countHTML - 1;
+
+                totalSumProducts.forEach((element) => {
+                  if (element.dataset.id === count.dataset.id) {
+                    element.innerText = element.innerHTML - singleProPrice;
+
+                    totalShoppingSum.innerHTML = `${sum} €`;
+                  }
+                });
+              }
+            });
+
+            break;
+          }
+        }
+
+        saveProduct(existingShoppingCart);
+      });
+    });
+
+    plusButton.forEach((button) => {
+      button.addEventListener("click", () => {
+        let singleProPrice = button.dataset.price;
+
         let item = {
           id: button.dataset.id,
-          image: button.dataset.img,        
+          image: button.dataset.img,
           price: button.dataset.price,
-          title: button.dataset.title
-        }  
-        productCount.forEach(count => {
-          if(count.dataset.id === item.id){
-            const countHTML = parseInt(count.innerText)
-            count.innerHTML = countHTML + 1
+          title: button.dataset.title,
+        };
+        productCount.forEach((count) => {
+          if (count.dataset.id === item.id) {
+            const countHTML = parseInt(count.innerText);
+            count.innerHTML = countHTML + 1;
+
+            totalSumProducts.forEach((element) => {
+              if (element.dataset.id === count.dataset.id) {
+                let totalSumPrProduct = element.innerHTML;
+
+                singleProPrice = parseInt(singleProPrice);
+                totalSumPrProduct = parseInt(totalSumPrProduct);
+                element.innerText = totalSumPrProduct + singleProPrice;
+
+                let lastSum = (sum += singleProPrice);
+                totalShoppingSum.innerHTML = `${lastSum} €`;
+              }
+            });
           }
         });
-        
-      existingShoppingCart.push(item)
-      saveProduct(existingShoppingCart)
-      
-    })
+
+        existingShoppingCart.push(item);
+        saveProduct(existingShoppingCart);
+      });
+    });
   });
 }
